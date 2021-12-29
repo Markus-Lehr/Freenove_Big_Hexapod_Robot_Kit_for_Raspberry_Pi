29ec98e3800b73ac28afcef2931f502bca64a731
Markus Lehr <indiemarkus@gmail.com>
2021-12-29 17:19:02 +0100

    Add management flask server

--- Code/Autonomous/management.py
+++ Code/Autonomous/management.py
@@ -9,9 +9,13 @@ agent = Agent()
 @app.route('/move-motor', methods=['GET'])
 def move_motor():
     leg = int(request.args.get('leg'))
-    angle1 = float(request.args.get('angle1'))
-    angle2 = float(request.args.get('angle2'))
-    angle3 = float(request.args.get('angle3'))
+    angle1 = request.args.get('angle1')
+    angle2 = request.args.get('angle2')
+    angle3 = request.args.get('angle3')
+
+    angle1 = float(angle1) if angle1 is not None else None
+    angle2 = float(angle2) if angle2 is not None else None
+    angle3 = float(angle3) if angle3 is not None else None
     agent.set_angles(leg, [angle1, angle2, angle3])
     return 'Thank you!'
 
